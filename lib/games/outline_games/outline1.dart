66d53423bed043bee2a8ff77d66d459e2c98e196
migarakavishan <migarakavishan43@gmail.com>
2024-06-23 22:05:04 +0530

    outline level 1 game full ui done

--- lib/games/outline_games/outline1.dart
+++ lib/games/outline_games/outline1.dart
@@ -12,56 +12,84 @@ class _Outline1State extends State<Outline1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Drag and drop to correct shadow",
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-      body: Stack(
-        children: [
-          Positioned(
-            bottom: 50,
-            right: 100,
-            child: Draggable<String>(
-              data: "apple",
-              onDraggableCanceled: (velocity, offset) {},
-              feedback: Image.asset(
-                "assets/images/appleright.png",
-                height: 200,
-                opacity: const AlwaysStoppedAnimation(0.5),
-              ),
-              childWhenDragging: Container(),
-              child: Image.asset(
-                "assets/images/appleright.png",
-                height: 200,
-              ),
-            ),
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.white),
+          backgroundColor: const Color(0xFF4CA5C1),
+          title: const Text(
+            "Drag and drop to correct shadow",
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
           ),
-          Positioned(
-            top: 50,
-            left: 100,
-            child: DragTarget<String>(
-              onAcceptWithDetails: (data) {
-                setState(() {
-                  isDropped = true;
-                });
-              },
-              builder: (context, candidateData, rejectedData) {
-                return isDropped
-                    ? Image.asset(
-                        "assets/images/appleright.png",
-                        height: 200,
-                      )
-                    : Image.asset(
-                        "assets/images/appledarkright.png",
-                        height: 200,
-                      );
-              },
-            ),
-          )
-        ],
-      ),
-    );
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text(
+                "Level 1",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                height: 300,
+                decoration: const BoxDecoration(color: Color(0xFFAEAEAE)),
+                child: Stack(
+                  children: [
+                    Positioned(
+                        top: 50,
+                        left: 80,
+                        child: DragTarget<String>(
+                          onAcceptWithDetails: (data) {
+                            setState(() {
+                              isDropped = true;
+                            });
+                          },
+                          builder: (context, candidateData, rejectedData) {
+                            return isDropped
+                                ? Image.asset(
+                                    "assets/images/appleright.png",
+                                    height: 200,
+                                  )
+                                : Image.asset(
+                                    "assets/images/appledarkright.png",
+                                    height: 200,
+                                  );
+                          },
+                        ))
+                  ],
+                ),
+              ),
+              Container(
+                height: 300,
+                decoration: const BoxDecoration(color: Color(0xFFFFA7A7)),
+                child: Stack(
+                  children: [
+                    Positioned(
+                        bottom: 40,
+                        left: 80,
+                        child: Draggable<String>(
+                            data: "apple",
+                            feedback: Image.asset(
+                              "assets/images/appleright.png",
+                              height: 200,
+                              opacity: const AlwaysStoppedAnimation(0.5),
+                            ),
+                            childWhenDragging: Container(),
+                            child: Image.asset(
+                              "assets/images/appleright.png",
+                              height: 200,
+                            )))
+                  ],
+                ),
+              ),
+              const CircleAvatar(
+                child: Icon(Icons.arrow_right_alt_rounded),
+              )
+            ],
+          ),
+        ));
   }
 }
