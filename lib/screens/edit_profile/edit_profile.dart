08007cc8a43a8c76352109de1a613736ed56ae0b
migarakavishan <migarakavishan43@gmail.com>
2024-06-29 21:29:43 +0530

    edit profile page add

---
+++ lib/screens/edit_profile/edit_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({super.key});
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
