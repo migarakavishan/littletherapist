0dde88939f99e3953300596525a715086cb37480
migarakavishan <migarakavishan43@gmail.com>
2024-07-06 15:23:57 +0530

    add firebase to the project branch-patch-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:littletherapist/firebase_options.dart';
 import 'package:littletherapist/screens/splash_screen/splash_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
