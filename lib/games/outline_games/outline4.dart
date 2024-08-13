36ae46a7f6555e2481611682c13df0fdefa2043a
migarakavishan <migarakavishan43@gmail.com>
2024-08-13 21:54:45 +0530

    outline 4 start

---
+++ lib/games/outline_games/outline4.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Outline4 extends StatefulWidget {
+  const Outline4({super.key});
+
+  @override
+  State<Outline4> createState() => _Outline4State();
+}
+
+class _Outline4State extends State<Outline4> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
