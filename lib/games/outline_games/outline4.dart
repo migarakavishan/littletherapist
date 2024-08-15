2411766beeb995109df8c301c3d718fab56a0a2d
migarakavishan <migarakavishan43@gmail.com>
2024-08-15 23:41:21 +0530

    "Added AppBar to Scaffold in Outline4 widget"

--- lib/games/outline_games/outline4.dart
+++ lib/games/outline_games/outline4.dart
@@ -10,6 +10,15 @@ class Outline4 extends StatefulWidget {
 class _Outline4State extends State<Outline4> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.white),
+          backgroundColor: const Color(0xFF4CA5C1),
+          title: const Text(
+            "Drag and drop to correct shadow",
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+    );
   }
 }
