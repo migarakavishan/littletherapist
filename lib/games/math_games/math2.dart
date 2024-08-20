756dcac68e59276291d391bf0bc512eda8409897
migarakavishan <migarakavishan43@gmail.com>
2024-08-20 20:34:27 +0530

    math 2 add

---
+++ lib/games/math_games/math2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Math2 extends StatefulWidget {
+  const Math2({super.key});
+
+  @override
+  State<Math2> createState() => _Math2State();
+}
+
+class _Math2State extends State<Math2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
