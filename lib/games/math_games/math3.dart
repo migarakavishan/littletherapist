76e4862ed510d776172ace3425b19ff1129eac93
Migara Kavishan <103320234+migarakavishan@users.noreply.github.com>
2024-08-23 15:12:15 +0530

    Update math3.dart

--- lib/games/math_games/math3.dart
+++ lib/games/math_games/math3.dart
@@ -10,6 +10,6 @@ class Math3 extends StatefulWidget {
 class _Math3State extends State<Math3> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
