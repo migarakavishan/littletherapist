c9481176c5dd32e2400d4d8b5d9d73a340f86ec4
migarakavishan <migarakavishan43@gmail.com>
2024-08-25 00:59:43 +0530

    "Added Math5 game level with timer, score tracking, and drag-and-drop functionality; updated DemoData to include Math5 level."

--- lib/games/math_games/math5.dart
+++ lib/games/math_games/math5.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:littletherapist/providers/math_score_provide.dart';
+import 'package:provider/provider.dart';
 
 class Math5 extends StatefulWidget {
   const Math5({super.key});
@@ -8,8 +12,366 @@ class Math5 extends StatefulWidget {
 }
 
 class _Math5State extends State<Math5> {
+  bool isNum7Dropped = false;
+
+  late Timer _timer;
+  int _start = 60;
+  double _progress = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_start > 0) {
+          _start--;
+          _progress = _start / 60.0;
+        } else {
+          _timer.cancel();
+          navigateToNextOutline();
+          if (isNum7Dropped) {
+            completeMath();
+          } else {
+            // Handle what happens if the time runs out without both items correctly placed
+          }
+        }
+      });
+    });
+  }
+
+  void completeMath() {
+    if (_start > 0) {
+      Provider.of<MathScoreProvider>(context, listen: false).addScore(10);
+      navigateToNextOutline();
+    }
+    _timer.cancel();
+    navigateToNextOutline();
+  }
+
+  void navigateToNextOutline() {
+    // CustomNavigation2.nextPage2(
+    //     context, const Math5()); // Assuming there's another level
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: const Color(0xFF4CA5C1),
+        title: const Text(
+          "Give Correct Ansewer",
+          style: TextStyle(
+              fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: CircularProgressIndicator(
+                          value: _progress,
+                          backgroundColor: Colors.grey[300],
+                          color: Colors.blue,
+                          strokeWidth: 6,
+                        ),
+                      ),
+                      Text("${_start}s"),
+                    ],
+                  ),
+                  const Text(
+                    "Level 5",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 45,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.blue.shade500,
+                            borderRadius: const BorderRadiusDirectional.only(
+                                topStart: Radius.circular(10),
+                                bottomStart: Radius.circular(10))),
+                        child: const Center(
+                            child: Text(
+                          "Score",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                      ),
+                      Container(
+                        width: 45,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.blue.shade400,
+                            borderRadius: const BorderRadiusDirectional.only(
+                                topEnd: Radius.circular(10),
+                                bottomEnd: Radius.circular(10))),
+                        child: Center(
+                          child: Text(
+                              '${Provider.of<MathScoreProvider>(context).score}',
+                              style: const TextStyle(color: Colors.white)),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 500,
+              height: 450,
+              decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    )
+                  ]),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    "assets/images/appleright.png",
+                                    height: 60,
+                                  ),
+                                  Image.asset(
+                                    "assets/images/appleright.png",
+                                    height: 60,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    "assets/images/appleright.png",
+                                    height: 60,
+                                  ),
+                                  Image.asset(
+                                    "assets/images/appleright.png",
+                                    height: 60,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Text(
+                          "+",
+                          style: TextStyle(fontSize: 80, color: Colors.white),
+                        ),
+                        Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    "assets/images/appleright.png",
+                                    height: 60,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    "assets/images/appleright.png",
+                                    height: 60,
+                                  ),
+                                  Image.asset(
+                                    "assets/images/appleright.png",
+                                    height: 60,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "=",
+                        style: TextStyle(fontSize: 80, color: Colors.white),
+                      )
+                    ],
+                  ),
+                  Container(
+                    height: 150,
+                    width: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: DragTarget<String>(
+                      onWillAccept: (data) => data == "number7",
+                      onAccept: (data) {
+                        setState(() {
+                          isNum7Dropped = true;
+                          completeMath();
+                        });
+                      },
+                      builder: (context, candidateData, rejectedData) {
+                        return isNum7Dropped
+                            ? Image.asset(
+                                "assets/images/number7.png",
+                                height: 100,
+                              )
+                            : const SizedBox(
+                                height: 90,
+                                width: 90,
+                              );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 150,
+              width: 500,
+              decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    )
+                  ]),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Draggable<String>(
+                      data: "number7",
+                      feedback: Image.asset(
+                        "assets/images/number7.png",
+                        height: 90,
+                        width: 90,
+                        opacity: const AlwaysStoppedAnimation(0.5),
+                      ),
+                      childWhenDragging: const SizedBox(
+                        height: 90,
+                        width: 90,
+                      ),
+                      child: Image.asset(
+                        "assets/images/number7.png",
+                        height: 90,
+                        width: 90,
+                      )),
+                  Draggable<String>(
+                      data: "number4",
+                      feedback: Image.asset(
+                        "assets/images/number4.png",
+                        height: 90,
+                        width: 90,
+                        opacity: const AlwaysStoppedAnimation(0.5),
+                      ),
+                      childWhenDragging: const SizedBox(
+                        height: 90,
+                        width: 90,
+                      ),
+                      child: Image.asset(
+                        "assets/images/number4.png",
+                        height: 90,
+                        width: 90,
+                      )),
+                  Draggable<String>(
+                      data: "number5",
+                      feedback: Image.asset(
+                        "assets/images/number5.png",
+                        height: 90,
+                        width: 90,
+                        opacity: const AlwaysStoppedAnimation(0.5),
+                      ),
+                      childWhenDragging: const SizedBox(
+                        height: 90,
+                        width: 90,
+                      ),
+                      child: Image.asset(
+                        "assets/images/number5.png",
+                        height: 90,
+                        width: 90,
+                      )),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                // CustomNavigation2.nextPage2(context, const Math5());
+              },
+              child: const CircleAvatar(
+                child: Icon(Icons.arrow_right_alt_rounded),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/utils/demo_data.dart
+++ lib/utils/demo_data.dart
@@ -3,6 +3,7 @@ import 'package:littletherapist/games/math_games/math1.dart';
 import 'package:littletherapist/games/math_games/math2.dart';
 import 'package:littletherapist/games/math_games/math3.dart';
 import 'package:littletherapist/games/math_games/math4.dart';
+import 'package:littletherapist/games/math_games/math5.dart';
 import 'package:littletherapist/games/outline_games/how_to_play_outline.dart';
 import 'package:littletherapist/games/outline_games/outline1.dart';
 import 'package:littletherapist/games/outline_games/outline2.dart';
@@ -72,6 +73,7 @@ class DemoData {
     MathModel(id: 2, name: "Level 2", widget: const Math2()),
     MathModel(id: 3, name: "Level 3", widget: const Math3()),
     MathModel(id: 4, name: "Level 4", widget: const Math4()),
+    MathModel(id: 5, name: "Level 5", widget: const Math5()),
   ];
 
   static List<DrawingPadModel> drawingPadmodel = [
