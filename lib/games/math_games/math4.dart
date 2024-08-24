78988bdabf28981cc7d5109485ca17d3bb8ea61a
migarakavishan <migarakavishan43@gmail.com>
2024-08-24 23:01:47 +0530

    ui changes

--- lib/games/math_games/math4.dart
+++ lib/games/math_games/math4.dart
@@ -10,6 +10,6 @@ class Math4 extends StatefulWidget {
 class _Math4State extends State<Math4> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
